d3ea4d96ab132de46e4683e1a841bb79b1c572ac
mikeroie-maker <mikeroie@yahoo.de>
2026-05-03 22:37:10 +0200

    app.js aktualisieren

--- app.js
+++ app.js
@@ -1,43 +1,99 @@
 let kunden = JSON.parse(localStorage.getItem("kunden")) || [];
+let objekte = JSON.parse(localStorage.getItem("objekte")) || [];
+let zahlungen = JSON.parse(localStorage.getItem("zahlungen")) || [];
 
 function speichern() {
   localStorage.setItem("kunden", JSON.stringify(kunden));
+  localStorage.setItem("objekte", JSON.stringify(objekte));
+  localStorage.setItem("zahlungen", JSON.stringify(zahlungen));
 }
 
 function anzeigen() {
-  let liste = document.getElementById("kundenListe");
-  liste.innerHTML = "";
+  kundenListe.innerHTML = kunden.map((k, i) =>
+    `<div class="item"><b>${k.name}</b><br>Miete: ${k.miete} € <button onclick="kundeLoeschen(${i})">Löschen</button></div>`
+  ).join("");
 
-  kunden.forEach((kunde, index) => {
-    liste.innerHTML += `
-      <div class="item">
-        <strong>${kunde.name}</strong><br>
-        Miete: ${kunde.miete} €<br>
-        <button onclick="loeschen(${index})">Löschen</button>
-      </div>
-    `;
-  });
+  objektListe.innerHTML = objekte.map((o, i) =>
+    `<div class="item"><b>${o.name}</b><br>Status: ${o.status} <button onclick="objektLoeschen(${i})">Löschen</button></div>`
+  ).join("");
+
+  zahlungenListe.innerHTML = zahlungen.map((z, i) =>
+    `<div class="item ${z.status}">
+      <b>${z.name}</b><br>
+      Monat: ${z.monat}<br>
+      Betrag: ${z.miete} €<br>
+      Status: ${z.status}
+      <br><button onclick="zahlungAendern(${i})">bezahlt/offen</button>
+    </div>`
+  ).join("");
 }
 
 function addKunde() {
-  let name = document.getElementById("name").value;
-  let miete = document.getElementById("miete").value;
-
-  if (!name || !miete) {
+  if (!name.value || !miete.value) {
     alert("Bitte Name und Miete eintragen");
     return;
   }
 
-  kunden.push({ name, miete });
+  kunden.push({
+    name: name.value,
+    miete: miete.value
+  });
+
+  name.value = "";
+  miete.value = "";
+
   speichern();
   anzeigen();
-
-  document.getElementById("name").value = "";
-  document.getElementById("miete").value = "";
 }
 
-function loeschen(index) {
-  kunden.splice(index, 1);
+function addObjekt() {
+  if (!objekt.value) {
+    alert("Bitte Objekt eintragen");
+    return;
+  }
+
+  objekte.push({
+    name: objekt.value,
+    status: "frei"
+  });
+
+  objekt.value = "";
+
+  speichern();
+  anzeigen();
+}
+
+function monatStart() {
+  const heute = new Date();
+  const monat = (heute.getMonth() + 1) + "/" + heute.getFullYear();
+
+  kunden.forEach(k => {
+    zahlungen.push({
+      name: k.name,
+      miete: k.miete,
+      monat: monat,
+      status: "offen"
+    });
+  });
+
+  speichern();
+  anzeigen();
+}
+
+function zahlungAendern(i) {
+  zahlungen[i].status = zahlungen[i].status === "offen" ? "bezahlt" : "offen";
+  speichern();
+  anzeigen();
+}
+
+function kundeLoeschen(i) {
+  kunden.splice(i, 1);
+  speichern();
+  anzeigen();
+}
+
+function objektLoeschen(i) {
+  objekte.splice(i, 1);
   speichern();
   anzeigen();
 }
@@ -47,6 +103,15 @@ function demo() {
     { name: "Max Mustermann", miete: "149" },
     { name: "Firma Schneider GmbH", miete: "220" }
   ];
+
+  objekte = [
+    { name: "Container 01", status: "vermietet" },
+    { name: "Container 02", status: "frei" },
+    { name: "Stellplatz A1", status: "vermietet" }
+  ];
+
+  zahlungen = [];
+
   speichern();
   anzeigen();
 }
